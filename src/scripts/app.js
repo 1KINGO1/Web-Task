714e0fff91ad3a26855808207cd32d6858ca99e7
KINGO <spental12345@gmail.com>
2024-09-22 13:08:58 +0300

    little changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,7 +8,6 @@ var serializeUserArray_1 = require("./serializeUserArray");
 var users = (0, serializeUserArray_1.connect)((0, serializeUserArray_1.serializeUserArray)(mock_1.randomUserMock), (0, serializeUserArray_1.serializeUserArray)(mock_1.additionalUsers));
 console.log(users);
 // users.map((user) => validateUser(user)).forEach(console.log);
-//
 // console.log("Filtered users:");
 // console.log(filterUsersBy(users,
 //     [
@@ -34,11 +33,31 @@ console.log(users);
 //     }
 // ))
 // console.log("Search user:");
-// console.log(searchBy(users, "August"))
+// console.log(searchOneBy(users, [
+//     {
+//         key: "age",
+//         greaterThanEquals: 65
+//     }
+// ]))
+// console.log(searchManyBy(users, [
+//     {
+//         key: "age",
+//         greaterThanEquals: 65,
+//         lessThanEquals: 70
+//     },
+//     {
+//         key: "gender",
+//         equals: "male"
+//     }
+// ]))
 // console.log("Get interest:");
 // console.log(getInterest(users, [
 //     {
 //         key: "age",
 //         greaterThan: 30
 //     },
+//     {
+//         key: "gender",
+//         equals: "other"
+//     }
 // ]) + "%");
